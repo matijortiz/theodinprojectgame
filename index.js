5e116d2256d66edabe6e09818300af254468630a
MatiO <mdoe@example.com>
2023-09-18 01:46:30 -0300

    create a function which checks who wins

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 
 // Begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 
-let computerChoose = ['rock', 'paper', 'scissor'];
+let computerChoose = ['rock', 'paper', 'scissors'];
 function getComputerChoice() {
     return ('computer has choose: ') + (computerChoose[Math.floor(Math.random() * 3)]);
 }
 
-//I'm adding this for test, to view the computer's choose on my console
+// I'm adding this for test, to view the computer's choose on my console //
 console.log(getComputerChoice());
+
+// Create a function which checks winner //
+
+function myWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a Tie";
+    } else if (
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'rock' && computerSelection == 'scissors')
+    ) {
+        return 'Player';
+    } else {
+        return 'Computer';
+    }
+}
